0f44827e317277d9c547f4f6eec93859c75b5996
Janyu <janyubhatt@gmail.com>
2012-06-30 16:37:27 -0400

    a

--- src/scripts/behavior.js
+++ src/scripts/behavior.js
@@ -1,44 +1,45 @@
+$(document).ready(function () {
 
-$(document).ready(function() {
 
-    
-    
+
     $("#AminoTable tbody").toggle();
 
-    $("#AminoTable th").hover(function() {
-    
+    $("#AminoTable th").hover(function () {
+
         $("#AminoTable tbody").toggle('fast');
-    
+
     });
 
-	// whenever somebody clicks on 'button1', we want the stats to be recalculated and displayed
-    $("#button1").click(function() {
+    // whenever somebody clicks on 'button1', we want the stats to be recalculated and displayed
+    $("#button1").click(function () {
 
-            // this is a jQuery selector:  it finds the DOM element with id = 'protein'
+        // this is a jQuery selector:  it finds the DOM element with id = 'protein'
         var sequence = $("#protein").val(),
             // this grabs some data from our model 
             aaPercs = Statistics.runWeights(sequence),
             aa,
             // this jQuery selector gets the 'tbody' element of the element with id = 'AminoTable'
             tbody = $("#AminoTable tbody"),
-            acidseq=Statistics.loadSequence(sequence);
-            weight=$("#weight"),
-            length=$("#length"),
-            hydrophilic=$("#hydrophilic"),
-            hydrophobic=$("#hydrophobic"),
-            numHydrophobic=(Statistics.calcNumHydrophilic(acidseq))/acidseq.length,
-            numHydrophilic=(Statistics.calcNumHydrophobic(acidseq))/acidseq.length;
-        weight.text((Statistics.getMolecularWeight(acidseq)).toPrecision(6));
+            acidseq = Statistics.loadSequence(sequence),
+            weight = $("#weight"),
+            length = $("#length"),
+            hydrophilic = $("#hydrophilic"),
+            hydrophobic = $("#hydrophobic"),
+            numHydrophobic = Statistics.calcNumHydrophilic(acidseq)*100 / acidseq.length,
+            numHydrophilic = Statistics.calcNumHydrophobic(acidseq)*100 / acidseq.length,
+            perHydrophobic = numHydrophobic.toPrecision(4),
+            perHydrophilic = numHydrophilic.toPrecision(4);
+        weight.text(Statistics.getMolecularWeight(acidseq).toPrecision(6));
         length.text(sequence.length);
-        hydrophilic.text(numHydrophobic.toPrecision(4)*100);
-        hydrophobic.text(numHydrophilic.toPrecision(4)*100);
+        hydrophilic.text(perHydrophilic);
+        hydrophobic.text(perHydrophobic);
         // get rid of the old data ...
         tbody.empty();
         // and and in the new
-        for(aa in aaPercs) {
-        	// make one row for each amino acid
+        for (aa in aaPercs) {
+            // make one row for each amino acid
             tbody.append("<tr><td>" + aa + "</td><td>" + aaPercs[aa] + "</td></tr>");
         }
-       
+
     });
 });
